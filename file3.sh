a2badc55ae17cedbeef4098189e1833a533c39b2
madesh89 <bm.madesha89@gmail.com>
2024-07-28 15:15:17 +0530

    if else condition

--- file3.sh
+++ file3.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-for i in {1..5}
-do
-    echo $i
-done
+read -p "Enter the variable a and b: " a 
+
+read b
+
+if [$a -gt $b]
+then
+	echo "$a is greater than $b"
+else
+	echo "$b is greater than $a"
+
+fi
