ebb4d007a60a876aa803df00e7cbed0bc47e01c5
madesh89 <bm.madesha89@gmail.com>
2024-07-28 16:16:18 +0530

    Greater number among three numbers

--- file3.sh
+++ file3.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-for i in {1..5}
-do
-    echo $i
-done
+echo " Enter the values of a , b and c"
+
+read a
+read b
+read c
+
+if [ $a -gt $b ] && [ $a -gt $c ] 
+then 
+	echo "$a is greater"
+elif [ $b -gt $c ] && [ $b -gt $a ]
+then
+	echo "$b is greater"
+else
+	echo "$c greater"
+fi
+
